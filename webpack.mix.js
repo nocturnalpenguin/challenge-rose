712ea0cce48a5d183e5950abeede2a0efc58a067
Nika Makharadze <makharadze.nika@gmail.com>
2020-06-09 05:33:00 +0400

    returned to public

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require('laravel-mix')
 
 mix
-    .js('src/js/app.js', 'dist/')
-    .sass('src/css/app.scss', 'dist/')
+    .js('src/js/app.js', 'public/')
+    .sass('src/css/app.scss', 'public/')
